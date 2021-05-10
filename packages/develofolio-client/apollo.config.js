9811d3b8e7017c302cb3e932680e16893bc1f080
ChoJongHoon <chojonghoon@kakao.com>
2021-05-10 22:58:30 +0900

    feat: 서버에서 스키마 가져와서 자동완성

---
+++ packages/develofolio-client/apollo.config.js
@@ -0,0 +1,11 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config()
+
+module.exports = {
+	client: {
+		service: {
+			name: 'develofolio',
+			url: process.env.NEXT_PUBLIC_GRAPHQL_HOST,
+		},
+	},
+}
