750e9f2ef32a1f28b389df42ac2c72a94e4a5dde
ChoJongHoon <chojonghoon@kakao.com>
2021-05-15 21:33:41 +0900

    test: 에디터 기본값 임시 설정

--- packages/develofolio-client/src/components/editor/editor.tsx
+++ packages/develofolio-client/src/components/editor/editor.tsx
@@ -15,21 +15,77 @@ export function Editor() {
 	)
 
 	const [value, setValue] = useState<Descendant[]>([
+		{ type: 'heading-one', children: [{ text: '이채영 (@Luna Lee)' }] },
 		{
 			type: 'paragraph',
+			children: [{ text: '안녕하세요 프론트엔드 엔지니어 이채영입니다.' }],
+		},
+		{
+			type: 'bulleted-list',
 			children: [
-				{ text: ' ' },
 				{
-					type: 'icon',
-					name: 'React',
-					shortname: 'react',
-					url: 'https://facebook.github.io/react/',
-					file: 'react.svg',
-					children: [{ text: '' }],
+					type: 'list-item',
+					children: [
+						{ text: '' },
+						{
+							type: 'icon',
+							name: 'React',
+							shortname: 'react',
+							url: 'https://facebook.github.io/react/',
+							file: 'react.svg',
+							children: [{ text: '' }],
+						},
+						{ text: ' React' },
+					],
 				},
-				{ text: ' ' },
+				{
+					type: 'list-item',
+					children: [
+						{ text: '' },
+						{
+							type: 'icon',
+							name: 'Apollo',
+							shortname: 'apollostack',
+							url: 'http://www.apollostack.com/',
+							file: 'apollostack.svg',
+							children: [{ text: '' }],
+						},
+						{ text: ' Apollo client' },
+					],
+				},
+				{
+					type: 'list-item',
+					children: [
+						{ text: '' },
+						{
+							type: 'icon',
+							name: 'Typescript',
+							shortname: 'typescript',
+							url: 'https://www.typescriptlang.org/',
+							file: 'typescript-icon.svg',
+							children: [{ text: '' }],
+						},
+						{ text: ' TypeScript' },
+					],
+				},
+				{
+					type: 'list-item',
+					children: [
+						{ text: '' },
+						{
+							type: 'icon',
+							name: 'GraphQL',
+							shortname: 'graphql',
+							url: 'http://graphql.org/',
+							file: 'graphql.svg',
+							children: [{ text: '' }],
+						},
+						{ text: ' GraphQL' },
+					],
+				},
 			],
 		},
+		{ type: 'paragraph', children: [{ text: '' }] },
 	])
 
 	const onChnage = useCallback(
