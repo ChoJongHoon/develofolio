968d8b254bf8f2181971059b51afb686cbb23fcd
ChoJongHoon <chojonghoon@kakao.com>
2021-05-16 20:06:31 +0900

    css: font

--- packages/develofolio-client/src/styles/global-styles.tsx
+++ packages/develofolio-client/src/styles/global-styles.tsx
@@ -9,6 +9,13 @@ export const globalStyle = (
 			html {
 				font-size: 16px;
 			}
+
+			body {
+				font-family: -apple-system, BlinkMacSystemFont, 'Helvetica Neue',
+					'Apple SD Gothic Neo', 'Malgun Gothic', '맑은 고딕', 나눔고딕,
+					'Nanum Gothic', 'Noto Sans KR', 'Noto Sans CJK KR', arial, 돋움, Dotum,
+					Tahoma, Geneva, sans-serif;
+			}
 		`}
 	/>
 )
