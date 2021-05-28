e0a920c54a65d444c29134b59dfe04281d80e469
ChoJongHoon <chojonghoon@kakao.com>
2021-05-28 22:19:50 +0900

    feat: 가입후 init template 제공

--- packages/develofolio-client/src/components/editor/hooks/use-editor.ts
+++ packages/develofolio-client/src/components/editor/hooks/use-editor.ts
@@ -9,28 +9,25 @@ import {
 import { useDebounceEffect } from '~/lib/hooks/use-debounce-effect'
 import { setLoading, setSaved, setSaving } from '../editor.reducer'
 import { useAsync } from 'react-use'
+import { nanoid } from 'nanoid'
+
+const getInitContent = (): Descendant[] => [
+	{
+		key: nanoid(),
+		type: 'banner',
+		children: [
+			{ key: nanoid(), type: 'banner-name', children: [{ text: '' }] },
+			{ key: nanoid(), type: 'banner-tagline', children: [{ text: '' }] },
+			{ key: nanoid(), type: 'banner-bio', children: [{ text: '' }] },
+		],
+	},
+	{ key: nanoid(), type: 'paragraph', children: [{ text: '' }] },
+]
 
 export const useEditor = () => {
 	const dispatch = useDispatch()
 	const client = useApolloClient()
 	const [value, setValue] = useState<Descendant[]>([])
-
-	useAsync(async () => {
-		dispatch(setLoading(true))
-		const {
-			data: { myContent: initContent },
-		} = await client.query({ query: MyContentDocument })
-		if (initContent) {
-			setValue(initContent)
-		}
-		dispatch(setLoading(false))
-		dispatch(setSaved(true))
-	}, [])
-
-	useEffect(() => {
-		dispatch(setSaved(false))
-	}, [dispatch, value])
-
 	const onChange = useCallback((newValue: Descendant[]) => {
 		setValue(newValue)
 	}, [])
@@ -40,6 +37,32 @@ export const useEditor = () => {
 			content: value,
 		},
 	})
+
+	useAsync(async () => {
+		dispatch(setLoading(true))
+
+		const {
+			data: { myContent: initContent },
+		} = await client.query({ query: MyContentDocument })
+
+		if (initContent) {
+			setValue(initContent)
+		} else {
+			const content = getInitContent()
+			setValue(content)
+			onSave({
+				variables: {
+					content,
+				},
+			})
+		}
+		dispatch(setLoading(false))
+		dispatch(setSaved(true))
+	}, [])
+
+	useEffect(() => {
+		dispatch(setSaved(false))
+	}, [dispatch, value])
 
 	useDebounceEffect(
 		() => {
