02ded4dff8ad5a0acac208ca8fbe8a45510033e9
ChoJongHoon <chojonghoon@kakao.com>
2021-07-17 18:19:05 +0900

    paragraph 에서 포커스 없을 때 placeholder 보여짐

--- packages/develofolio-client/src/modules/editor/elements/paragraph.tsx
+++ packages/develofolio-client/src/modules/editor/elements/paragraph.tsx
@@ -1,14 +1,23 @@
 import OpenColor from 'open-color'
+import { useSelected } from 'slate-react'
 import { StyleObject, useStyletron } from 'styletron-react'
 import { CustomRenderElementProps, ParagraphElement } from '../custom-types'
+import { Placeholder } from '../placeholder/placeholder'
 
 export const Paragraph = ({
 	attributes,
 	children,
+	element,
 }: CustomRenderElementProps<ParagraphElement>) => {
 	const [css] = useStyletron()
+	const selected = useSelected()
 	return (
 		<p {...attributes} className={css(styles)}>
+			{selected && (
+				<Placeholder element={element}>
+					마크다운을 이용해 작성해보세요!
+				</Placeholder>
+			)}
 			{children}
 		</p>
 	)
