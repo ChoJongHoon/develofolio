c7990e59f52b68c35243758df2841adaf2ceba8e
ChoJongHoon <chojonghoon@kakao.com>
2021-07-21 22:50:56 +0900

    editor layout 생성

---
+++ packages/develofolio-client/src/layouts/editor-layout.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { useStyletron } from 'styletron-react'
+import { EditorSidebar } from '~/modules/editor/editor-sidebar'
+
+interface EditorLayout {
+	children?: React.ReactNode
+}
+
+export const EditorLayout = ({ children }: EditorLayout) => {
+	const [css] = useStyletron()
+
+	return (
+		<div>
+			<div
+				className={css({
+					minHeight: '100vh',
+				})}
+			>
+				<div
+					className={css({
+						position: 'fixed',
+						top: '0px',
+						left: '0px',
+						bottom: '0px',
+						width: '72px',
+					})}
+				>
+					<EditorSidebar />
+				</div>
+				<div
+					className={css({
+						marginLeft: '72px',
+						width: 'calc(100% - 72px)',
+					})}
+				>
+					{children}
+				</div>
+			</div>
+		</div>
+	)
+}
--- packages/develofolio-client/src/pages/edit/index.tsx
+++ packages/develofolio-client/src/pages/edit/index.tsx
@@ -1,10 +1,9 @@
 import { NextPage } from 'next'
 import { PageEditor } from '~/modules/editor/page-editor'
-import { EditorSidebar } from '~/modules/editor/editor-sidebar'
 import { MyPageDocument, PagePartsFragment } from '~/graphql/document.generated'
 import { withAuthSsr } from '~/apollo/utils/with-auth-ssr'
-import { useStyletron } from 'styletron-react'
 import { ROUTE_EDIT_NEW } from '~/routes'
+import { EditorLayout } from '~/layouts/editor-layout'
 
 interface EditProps {
 	page: PagePartsFragment
@@ -34,36 +33,10 @@ export const getServerSideProps = withAuthSsr<EditProps>(
 )
 
 const Edit: NextPage<EditProps> = ({ page }) => {
-	const [css] = useStyletron()
 	return (
-		<div
-			className={css({
-				minHeight: '100vh',
-			})}
-		>
-			<div
-				className={css({
-					position: 'fixed',
-					top: '0px',
-					left: '0px',
-					bottom: '0px',
-					width: '72px',
-				})}
-			>
-				<EditorSidebar />
-			</div>
-			<div
-				className={css({
-					marginLeft: '72px',
-					width: 'calc(100% - 72px)',
-				})}
-			>
-				<PageEditor
-					className={css({ flex: '1 1 0' })}
-					initialContent={page.content}
-				/>
-			</div>
-		</div>
+		<EditorLayout>
+			<PageEditor initialContent={page.content} />
+		</EditorLayout>
 	)
 }
 
