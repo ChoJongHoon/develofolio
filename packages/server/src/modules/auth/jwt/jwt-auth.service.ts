7ecba0e3f23ce8a3479e51cecc28759cd659aaed
ChoJongHoon <chojonghoon@kakao.com>
2021-08-04 21:56:55 +0900

    jwt domain 설정

--- packages/server/src/modules/auth/jwt/jwt-auth.service.ts
+++ packages/server/src/modules/auth/jwt/jwt-auth.service.ts
@@ -35,22 +35,43 @@ export class JwtAuthService {
 	}
 
 	setAccessToken(res: Response, accessToken: string) {
-		res.cookie('access_token', accessToken, { httpOnly: true })
+		res.cookie('access_token', accessToken, {
+			httpOnly: true,
+			domain:
+				process.env.NODE_ENV === 'production'
+					? '.develofolio.com'
+					: 'localhost',
+		})
 	}
 
 	setRefreshToken(res: Response, refreshToken: string) {
 		res.cookie('refresh_token', refreshToken, {
 			httpOnly: true,
 			path: '/jwt/refresh',
+			domain:
+				process.env.NODE_ENV === 'production'
+					? '.develofolio.com'
+					: 'localhost',
 		})
 	}
 
 	removeToken(res: Response) {
-		res.cookie('access_token', '', { httpOnly: true, maxAge: 0 })
+		res.cookie('access_token', '', {
+			httpOnly: true,
+			maxAge: 0,
+			domain:
+				process.env.NODE_ENV === 'production'
+					? '.develofolio.com'
+					: 'localhost',
+		})
 		res.cookie('refresh_token', '', {
 			httpOnly: true,
 			path: '/jwt/refresh',
 			maxAge: 0,
+			domain:
+				process.env.NODE_ENV === 'production'
+					? '.develofolio.com'
+					: 'localhost',
 		})
 	}
 }
