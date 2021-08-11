dee6f6eb5b7ee865bfcb2b2da95f3bdede2ab341
ChoJongHoon <chojonghoon@kakao.com>
2021-08-11 23:15:58 +0900

    apollo.config.js 가 localSchemaFile 을 참조하도록 설정

--- packages/client/apollo.config.js
+++ packages/client/apollo.config.js
@@ -1,11 +1,8 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-require('dotenv').config()
-
 module.exports = {
 	client: {
 		service: {
 			name: 'develofolio',
-			url: `${process.env.NEXT_PUBLIC_SERVER_HOST}/graphql`,
+			localSchemaFile: './packages/server/schema.graphql',
 		},
 		excludes: ['**/*.generated.*'],
 	},
