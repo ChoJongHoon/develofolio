cb25917c1d3da95a532e975fffbbeec61efb9ee0
ChoJongHoon <chojonghoon@kakao.com>
2021-09-29 21:58:37 +0900

    fix(client): LogoPicker 그림자 css 추가

--- packages/client/src/modules/editor/logo/inline-logo-picker.tsx
+++ packages/client/src/modules/editor/logo/inline-logo-picker.tsx
@@ -127,6 +127,8 @@ const boxStyles: StyleObject = {
 	flexDirection: 'column',
 	borderRadius: '8px',
 	backgroundColor: OpenColor.white,
+	boxShadow:
+		'rgba(15, 15, 15, 0.05) 0px 0px 0px 1px, rgba(15, 15, 15, 0.1) 0px 3px 6px, rgba(15, 15, 15, 0.2) 0px 9px 24px',
 }
 
 const gridWrapperStyles: StyleObject = {
