ec205c7911dbbed05a33a2f6da164eeabf971f52
ChoJongHoon <chojonghoon@kakao.com>
2021-10-25 23:29:58 +0900

    fix(client): 포트폴리오 페이지 meta 태그 중복 버그 수정

--- packages/client/src/layouts/basic-layout.tsx
+++ packages/client/src/layouts/basic-layout.tsx
@@ -12,6 +12,7 @@ import { gsap } from 'gsap'
 import { linkStyles } from '~/styles/styles'
 import { Icon } from '~/components/icon'
 import { useRouterLoading } from '~/hooks/use-router-loading'
+import Head from 'next/head'
 
 interface BasicLayoutOverrides {
 	main?: {
@@ -67,145 +68,160 @@ export const BasicLayout = ({ children, overrides }: BasicLayoutProps) => {
 	}, [])
 
 	return (
-		<div
-			className={css({
-				display: 'flex',
-				flexDirection: 'column',
-				minHeight: '100vh',
-			})}
-		>
-			<header
-				ref={headerRef}
-				className={css(
-					merge<StyleObject, StyleObject>(
-						{
-							...padding('16px'),
-							display: 'flex',
-							justifyContent: 'space-between',
-							alignItems: 'center',
-							position: 'sticky',
-							top: '0px',
-							zIndex: 100,
-							backgroundColor: 'rgba(255,255,255,0)',
-							boxShadow: '0px 20px 40px 0px rgba(0,0,0,0)',
-						},
-						overrides?.header?.style || {}
-					)
-				)}
-			>
-				<Link href={ROUTE_HOME}>
-					<a>
-						<Logo
-							ref={logoRef}
-							className={css({
-								height: '16px',
-								transform: 'scale(1.25)',
-								transformOrigin: 'left center',
-							})}
-						/>
-					</a>
-				</Link>
-				<Link href={ROUTE_LOGIN} passHref>
-					<PrimaryButton
-						$as="a"
-						size="compact"
-						isLoading={loading}
-						overrides={{
-							BaseButton: {
-								style: {
-									...borderRadius('top', '8px'),
-									...borderRadius('bottom', '8px'),
-								},
-							},
-						}}
-					>
-						시작하기
-					</PrimaryButton>
-				</Link>
-			</header>
-			<main
-				className={css(
-					merge<StyleObject, StyleObject>(
-						{
-							flexGrow: 1,
-							flexShrink: 1,
-							position: 'relative',
-						},
-						overrides?.main?.style || {}
-					)
-				)}
-			>
-				{children}
-			</main>
-			<footer
+		<>
+			<Head>
+				<meta property="og:url" content="https://develofolio.com" />
+				<meta property="og:type" content="website" />
+				<meta property="og:title" content="DeveloFolio" />
+				<meta
+					property="og:description"
+					content="디벨로폴리오 - 개발자들의 포트폴리오 에디터"
+				/>
+				<meta
+					property="og:image"
+					content="https://images.develofolio.com/og-image.png"
+				/>
+			</Head>
+			<div
 				className={css({
-					...padding('16px', '32px'),
 					display: 'flex',
-					justifyContent: 'space-between',
-					backgroundColor: OpenColor.gray[1],
+					flexDirection: 'column',
+					minHeight: '100vh',
 				})}
 			>
-				<div className={css({ display: 'flex', alignItems: 'center' })}>
-					<LabelXSmall
-						color={OpenColor.gray[6]}
-						className={css({
-							marginRight: '16px',
-						})}
-					>
-						©DeveloFolio
-					</LabelXSmall>
-					<Link href={ROUTE_TERMS} passHref>
-						<LabelXSmall
-							as="a"
-							className={css({
-								...linkStyles,
-							})}
-							color={OpenColor.gray[6]}
-						>
-							이용약관
-						</LabelXSmall>
-					</Link>
-					<span
-						className={css({
-							display: 'block',
-							width: '24px',
-							textAlign: 'center',
-							color: OpenColor.gray[6],
-						})}
-					>
-						･
-					</span>
-					<Link href={ROUTE_PRIVACY} passHref>
-						<LabelXSmall
-							as="a"
-							className={css({
-								...linkStyles,
-							})}
-							color={OpenColor.gray[6]}
-						>
-							개인정보 처리방침
-						</LabelXSmall>
-					</Link>
-				</div>
-				<div>
-					<a
-						href="https://github.com/ChoJongHoon/develofolio"
-						target="_blank"
-						rel="noreferrer"
-						className={css({
-							cursor: 'pointer',
-							opacity: 0.6,
-							transitionProperty: 'opacity',
-							transitionDuration: '0.2s',
-							':hover': {
-								opacity: 1,
+				<header
+					ref={headerRef}
+					className={css(
+						merge<StyleObject, StyleObject>(
+							{
+								...padding('16px'),
+								display: 'flex',
+								justifyContent: 'space-between',
+								alignItems: 'center',
+								position: 'sticky',
+								top: '0px',
+								zIndex: 100,
+								backgroundColor: 'rgba(255,255,255,0)',
+								boxShadow: '0px 20px 40px 0px rgba(0,0,0,0)',
 							},
-						})}
-					>
-						<Icon type="Github2" size={24} color={OpenColor.gray[8]} />
-					</a>
-				</div>
-			</footer>
-		</div>
+							overrides?.header?.style || {}
+						)
+					)}
+				>
+					<Link href={ROUTE_HOME}>
+						<a>
+							<Logo
+								ref={logoRef}
+								className={css({
+									height: '16px',
+									transform: 'scale(1.25)',
+									transformOrigin: 'left center',
+								})}
+							/>
+						</a>
+					</Link>
+					<Link href={ROUTE_LOGIN} passHref>
+						<PrimaryButton
+							$as="a"
+							size="compact"
+							isLoading={loading}
+							overrides={{
+								BaseButton: {
+									style: {
+										...borderRadius('top', '8px'),
+										...borderRadius('bottom', '8px'),
+									},
+								},
+							}}
+						>
+							시작하기
+						</PrimaryButton>
+					</Link>
+				</header>
+				<main
+					className={css(
+						merge<StyleObject, StyleObject>(
+							{
+								flexGrow: 1,
+								flexShrink: 1,
+								position: 'relative',
+							},
+							overrides?.main?.style || {}
+						)
+					)}
+				>
+					{children}
+				</main>
+				<footer
+					className={css({
+						...padding('16px', '32px'),
+						display: 'flex',
+						justifyContent: 'space-between',
+						backgroundColor: OpenColor.gray[1],
+					})}
+				>
+					<div className={css({ display: 'flex', alignItems: 'center' })}>
+						<LabelXSmall
+							color={OpenColor.gray[6]}
+							className={css({
+								marginRight: '16px',
+							})}
+						>
+							©DeveloFolio
+						</LabelXSmall>
+						<Link href={ROUTE_TERMS} passHref>
+							<LabelXSmall
+								as="a"
+								className={css({
+									...linkStyles,
+								})}
+								color={OpenColor.gray[6]}
+							>
+								이용약관
+							</LabelXSmall>
+						</Link>
+						<span
+							className={css({
+								display: 'block',
+								width: '24px',
+								textAlign: 'center',
+								color: OpenColor.gray[6],
+							})}
+						>
+							･
+						</span>
+						<Link href={ROUTE_PRIVACY} passHref>
+							<LabelXSmall
+								as="a"
+								className={css({
+									...linkStyles,
+								})}
+								color={OpenColor.gray[6]}
+							>
+								개인정보 처리방침
+							</LabelXSmall>
+						</Link>
+					</div>
+					<div>
+						<a
+							href="https://github.com/ChoJongHoon/develofolio"
+							target="_blank"
+							rel="noreferrer"
+							className={css({
+								cursor: 'pointer',
+								opacity: 0.6,
+								transitionProperty: 'opacity',
+								transitionDuration: '0.2s',
+								':hover': {
+									opacity: 1,
+								},
+							})}
+						>
+							<Icon type="Github2" size={24} color={OpenColor.gray[8]} />
+						</a>
+					</div>
+				</footer>
+			</div>
+		</>
 	)
 }
