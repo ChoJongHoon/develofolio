9f1b82ba6751e62c3e27e6fbc99e8185fc2f6926
ChoJongHoon <chojonghoon@kakao.com>
2022-04-22 18:32:25 +0900

    feat(client): DevelofolioImage 컴포넌트 작성

---
+++ packages/client/src/components/develofolio-image.tsx
@@ -0,0 +1,16 @@
+import Image, { ImageLoader, ImageProps } from 'next/image'
+
+const loader: ImageLoader = ({ src, width, quality }) => {
+	const url = new URL(`${process.env.NEXT_PUBLIC_IMAGES_V2_HOST}/${src}`)
+
+	url.searchParams.set('w', width.toString())
+	if (quality) {
+		url.searchParams.set('q', quality.toString())
+	}
+
+	return url.toString()
+}
+
+export const DevelofolioImage = ({ alt, ...props }: ImageProps) => {
+	return <Image alt={alt} loader={loader} {...props} />
+}
